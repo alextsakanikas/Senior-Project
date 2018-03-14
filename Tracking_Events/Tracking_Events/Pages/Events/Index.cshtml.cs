83baf5f1b40e490a33b3a338bddfc81b24e25413
Kevin Hu <kevinhu1996@gmail.com>
2018-03-14 18:29:16 -0400

    Fixed search

--- Tracking_Events/Tracking_Events/Pages/Events/Index.cshtml.cs
+++ Tracking_Events/Tracking_Events/Pages/Events/Index.cshtml.cs
@@ -24,7 +24,9 @@ namespace Tracking_Events.Pages.Events
         public IList<Event> Event { get; set; }
 
         public string CurrentSort { get; set; }
-        public string CurrentSearch { get; set; }
+        public string CurrentVenueSearch { get; set; }
+        public string CurrentGenreSearch { get; set; }
+        public string CurrentZipSearch { get; set; }
 
         #region Sorting Purposes
         public string EventNameSort { get; set; }
@@ -33,7 +35,7 @@ namespace Tracking_Events.Pages.Events
         public string EndTimeSort { get; set; }
         #endregion
 
-        public async Task OnGetAsync(string sortOrder, string searchString, string statusMessage)
+        public async Task OnGetAsync(string sortOrder, string searchVenue, string searchGenre, string searchZip, string statusMessage)
         {
             EventNameSort = String.IsNullOrEmpty(sortOrder) ? "event_name_desc" : "";
             VenueNameSort = sortOrder == "Venue" ? "venue_desc" : "Venue";
@@ -51,10 +53,20 @@ namespace Tracking_Events.Pages.Events
             IQueryable<Event> events = _context.Event.Include(ev => ev.Venue).Include(ev => ev.Rsvps).AsQueryable();
 
             #region Filtering
-            CurrentSearch = searchString;
-            if (!String.IsNullOrEmpty(searchString))
+            CurrentVenueSearch = searchVenue;
+            CurrentGenreSearch = searchGenre;
+            CurrentZipSearch = searchZip;
+            if (!String.IsNullOrEmpty(searchVenue))
             {
-                events = events.Where(s => s.Venue.VenueName.Contains(searchString) || s.Genre.Contains(searchString) || s.Venue.Zip.ToString().Equals(searchString));
+                events = events.Where(s => s.Venue.VenueName.Contains(searchVenue));
+            }
+            if (!String.IsNullOrEmpty(searchGenre))
+            {
+                events = events.Where(s => s.Genre.Contains(searchGenre));
+            }
+            if (!String.IsNullOrEmpty(searchZip))
+            {
+                events = events.Where(s => s.Venue.Zip.ToString().Equals(searchZip));
             }
             #endregion
 
